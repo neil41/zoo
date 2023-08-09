4bd2cb8b87091ab161a910a822813a943fea358f
neil41 <pphrarocn@gmail.com>
2023-08-09 21:44:04 +0700

    create : main version 3

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,34 @@ void main() {
 
 class MyApp extends StatelessWidget {
   var name = "ddv";
+  aa() {
+    int a = 0;
+  }
+
+  a1() {
+    int a = 0;
+  }
+
+  a2() {
+    int a = 0;
+  }
+
+  a3() {
+    int a = 0;
+  }
+
+  a4() {
+    int a = 0;
+  }
+
+  a5() {
+    int a = 0;
+  }
+
+  a6() {
+    int a = 0;
+  }
+
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
